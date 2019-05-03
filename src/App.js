946ce0daac9e6813d7b7f266a892837f677e9f73
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2019-05-03 07:29:25 +0300

    react forms

--- src/App.js
+++ src/App.js
@@ -3,6 +3,13 @@ import Component1 from "./functional/component1";
 import Container1 from "./containers/container1";
 
 class App extends Component {
+    // Alternative form approach
+    // constructor(props) {
+    //     super(props);
+    //     this.handleChange = this.handleChange.bind(this);
+    //     this.handleSubmit = this.handleSubmit.bind(this);
+    // }
+
     Arr1 = [
         { id: 1, text: "Text A", number: 10 },
         { id: 2, text: "Text B", number: 20 },
@@ -11,12 +18,35 @@ class App extends Component {
         { id: 5, text: "Text E", number: 50 }
     ];
 
+    state = {
+        value: ""
+    };
+
     RenderListItem = props => (
         <div>
             {props.item.text}
             <p>{props.item.number}</p>
         </div>
     );
+
+    handleChange = event => {
+        this.setState({ value: event.target.value });
+    };
+
+    handleSubmit = event => {
+        event.preventDefault();
+        console.log(this.state.value);
+    };
+
+    // Alternative form approach:
+    // handleChange = event => {
+    //     this.setState({ value: event.target.value });
+    // };
+
+    // handleSubmit = event => {
+    //     event.preventDefault();
+    //     console.log(this.state.value);
+    // };
 
     render() {
         return (
@@ -35,6 +65,14 @@ class App extends Component {
                     {this.Arr1.map(item => (
                         <this.RenderListItem key={item.id} item={item} />
                     ))}
+                </div>
+                <div>
+                    My form
+                    <form onSubmit={this.handleSubmit}>
+                        <label>Name</label>
+                        <input id="inputName" onChange={this.handleChange} type="text" />
+                        <button type="submit">Submit</button>
+                    </form>
                 </div>
             </div>
         );
