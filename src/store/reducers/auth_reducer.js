e267efb84450dddff4246cb0016436b88899a702
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2019-05-07 08:09:55 +0300

    setting up profile action creators

--- src/store/reducers/auth_reducer.js
+++ src/store/reducers/auth_reducer.js
@@ -1,7 +1,8 @@
 import * as ACTION_TYPES from "../actions/action_types";
 
 const initialState = {
-    isAuthenticated: false
+    isAuthenticated: false,
+    profile: null
 };
 
 const AuthReducer = (state = initialState, action) => {
@@ -15,6 +16,16 @@ const AuthReducer = (state = initialState, action) => {
             return {
                 ...state,
                 isAuthenticated: false
+            };
+        case ACTION_TYPES.ADD_PROFILE:
+            return {
+                ...state,
+                profile: action.payload
+            };
+        case ACTION_TYPES.REMOVE_PROFILE:
+            return {
+                ...state,
+                profile: null
             };
         default:
             return state;
